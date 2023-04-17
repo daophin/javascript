64b8b7b905157a8576c29ca5b8cc2b556197f6b7
daophin <129446146+daophin@users.noreply.github.com>
2023-04-17 13:05:10 -0300

    Att

---
+++ ex008/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { // Condição simples só com um IF
+    console.log(`Você utrapassou o limite de velocidade permitido!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança`)
